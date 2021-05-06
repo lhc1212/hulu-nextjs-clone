c70740a1fd33bd3188b685d610b09026aa57e424
lhc1212 <2020140051@mybcis.cn>
2021-05-06 12:15:58 +0800

    finished

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
-import requests from "../utils/requests";
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
@@ -19,7 +18,7 @@ export async function getServerSideProps(context) {
   };
 }
 
-export default function Home({ results }) {
+export default function Home({results}) {
 
   return (
     <div>
